284058d547123e6cf91b3b8bff97dbb0f8e8f9ed
IamMancini <alessio.mancini@stud.hslu.ch>
2023-04-08 13:57:58 +0200

    Loading dataset and creating url for titanic

--- Script_LN_Titanic.R
+++ Script_LN_Titanic.R
@@ -1,1 +1,21 @@
 #Dataset Titanic https://www.kaggle.com/c/titanic/data
+
+#loading packages
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+#Create URL for dataset
+url <- "https://raw.githubusercontent.com/IamMancini/rstudio-titanic/main/train.csv?token=GHSAT0AAAAAACBE3PV2QOQJFXOEROJJC2TSZBRKWLQ"
+
+#load Dataset
+titanic <- read.csv(url)
+
+
+#Show first rows of dataset to check
+head(titanic)
+
+#summary of dataset
+summary(titanic)
+
+

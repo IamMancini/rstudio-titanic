91474d5675e5817fdb117b4e0a6d7376dd91595c
IamMancini <alessio.mancini@stud.hslu.ch>
2023-04-27 22:47:26 +0200

    commit

--- Script_LN_Titanic.R
+++ Script_LN_Titanic.R
@@ -53,14 +53,58 @@ names(lusitania)
 #Change columne 0 from Lusitania to be the same as titanic "PersonalId"
 #Changes have been made directly inside the csv
 
-#Change Name in Titanic to Family name and first name
+
+
+
+
+#rename "Family name" to "Family_name" in Lusitania
+lusitania <- lusitania %>% rename(Family_name = `Family.name`)
+write.csv(lusitania, "C:/Users/aless/DASB/rstudio-titanic/LusitaniaManifest.csv", row.names = FALSE)
+
+
+#rename Personal name to first_name in Lusitania
+lusitania <- rename(lusitania, first_name = Personal.name)
+write.csv(lusitania, "C:/Users/aless/DASB/rstudio-titanic/LusitaniaManifest.csv", row.names = FALSE)
+
+
+#remove Title from Lusitania
+lusitania <- lusitania %>% 
+  select(-Title)
+write.csv(lusitania, "C:/Users/aless/DASB/rstudio-titanic/LusitaniaManifest.csv", row.names = FALSE)
+
+
+#Change Column for "Sex" from Lusitania
+lusitania <- lusitania %>%
+  select(PassengerId, Family_name, first_name, Sex, everything())
+
+
+#Change sequence
 titanic <- titanic %>%
-  mutate(`Family_name` = str_trim(str_split(Name, ",")[[1]][1]),
-         `first_name` = str_trim(str_split(Name, ",")[[1]][2]))
+  select(PassengerId, Name, Sex, Survived, everything())
+
+write.csv(titanic, "C:/Users/aless/DASB/rstudio-titanic/train.csv", row.names = FALSE)
+
+#Change the data from Titanic in Column "survived" to [yes = survived] and [no = not survived]
+titanic$Survived <- ifelse(titanic$Survived == 0, "no", "yes")
 write.csv(titanic, file = "C:/Users/aless/DASB/rstudio-titanic/train.csv", row.names = FALSE)
+
+#Change the name of the column "Fate" to "Survived" in Lusitania
+lusitania <- lusitania %>%
+  rename(Survived = Fate)
+write.csv(lusitania, "C:/Users/aless/DASB/rstudio-titanic/LusitaniaManifest.csv", row.names = FALSE)
+
+
+#Change the data from Lusitania in Column "Survival" to [yes =îf survived] and [no if not survived]
+lusitania$Survived <- ifelse(lusitania$Survived == "Lost", "no", "yes")
+
+show(lusitania)
+#save changes from lusitania
+write.csv(lusitania, "C:/Users/aless/DASB/rstudio-titanic/LusitaniaManifest.csv", row.names = FALSE)
+
+#-------------
+
+#Change Name in Titanic to Family name and first name
+
 
 #Delete the column Name in Titanic because its unnecessary
-titanic <- select(titanic, -Name)
-write.csv(titanic, file = "C:/Users/aless/DASB/rstudio-titanic/train.csv", row.names = FALSE)
-
 
